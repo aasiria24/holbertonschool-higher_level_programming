579d3bf486640c6a54d0a40ac04b5a23e2cc549f
aasiria24 <amaalmoasiri@gmail.com>
2026-01-14 07:33:31 +0000

    Adding the quote mark

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -12,6 +12,6 @@ if number < 0:
     if last_digit > 5:
         print("greater than 5")
     elif last_digit == 0:
-        print("0)
+        print("0")
         else:
         print("less than 6 and not 0")
