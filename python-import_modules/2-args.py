76180818c1c48ed1596420dca7eff75caa858266
aasiria24 <amaalmoasiri@gmail.com>
2026-01-15 16:46:22 +0000

    Fixing the output v3

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,16 +2,15 @@
 import sys
 
 if __name__ == "__main__":
+    argv = sys.argv
+    argc = len(argv) - 1
 
-    num_args = len(sys.argv) - 1
-    argv = sys.num_args
-
-    if num_args == 0:
+    if argc == 0:
         print("0 arguments.")
-    elif num_args == 1:
+    elif argc == 1:
         print("1 argument:")
     else:
-        print("{} arguments:".format(num_args))
+        print("{:d} arguments:".format(argc))
 
-        for i in range(1, len(argv[i])):
-            print("{:d}: {}".format(i, sys.argv[i]))
+    for i in range(1, len(argv)):
+        print("{:d}: {}".format(i, argv[i]))
