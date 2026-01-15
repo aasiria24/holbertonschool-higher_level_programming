feae8aa823636968797df1e864383371c11aade4
aasiria24 <amaalmoasiri@gmail.com>
2026-01-15 16:45:40 +0000

    Fixing the output v2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,6 +4,7 @@ import sys
 if __name__ == "__main__":
 
     num_args = len(sys.argv) - 1
+    argv = sys.num_args
 
     if num_args == 0:
         print("0 arguments.")
@@ -12,5 +13,5 @@ if __name__ == "__main__":
     else:
         print("{} arguments:".format(num_args))
 
-        for i in range(1, num_args + 1):
-            print("{}: {}".format(i, sys.argv[i]))
+        for i in range(1, len(argv[i])):
+            print("{:d}: {}".format(i, sys.argv[i]))
