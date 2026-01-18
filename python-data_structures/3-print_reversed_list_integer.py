322d3a76447324664c451582ab5cf8ec210b25fb
aasiria24 <amaalmoasiri@gmail.com>
2026-01-18 09:08:34 +0000

    Fixing pycodestyle

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
+
 def print_reversed_list_integer(my_list=[]):
     """Print all of the elements of a list in reverse order"""
     for num in reversed(my_list):
-            print("{:d}".format(num))
+        print("{:d}".format(num))
