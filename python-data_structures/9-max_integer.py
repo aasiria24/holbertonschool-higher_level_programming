617301b369c83fc4c2d293d2c265ae7ccf520fa6
aasiria24 <amaalmoasiri@gmail.com>
2026-01-18 11:51:53 +0000

    the spaces

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
-def max_integer(my_list=[]):
-    """finds the biggest integer of a list."""
-    if not my_list:
-    return None
 
-max_value = my_list[0]
+def max_integer(my_list=[]):
+    """Finds the biggest integer in a list."""
+    if not my_list:
+        return None
+
+    max_value = my_list[0]
     for num in my_list:
         if num > max_value:
             max_value = num
