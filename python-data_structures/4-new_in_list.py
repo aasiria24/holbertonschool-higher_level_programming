180345c186ef24155ac04b0b1b53cd1a6296ba5c
aasiria24 <amaalmoasiri@gmail.com>
2026-01-18 09:35:04 +0000

    Line 5 too long

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -2,7 +2,8 @@
 
 
 def new_in_list(my_list, idx, element):
-    """Replacing an element in a list at specific position without modifying the original list."""
+    """Replacing an element in a list at specific position
+    without modifying the original list."""
     if idx < 0 or idx >= len(my_list):
         return my_list[:]
 
