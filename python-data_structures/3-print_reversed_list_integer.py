f8ae6dea5fdce04b67a2909141e3d80d929fb9d2
aasiria24 <amaalmoasiri@gmail.com>
2026-01-18 08:44:33 +0000

    Fixing the syntax error

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -2,5 +2,5 @@
 
 def print_reversed_list_integer(my_list=[]):
     """Print all of the elements of a list in reverse order"""
-    for i in range(len(my_list) - 1, -1, -1:
+    for i in range(len(my_list) -1, -1, -1:
             print("{:d}".format(my_list[i]))
