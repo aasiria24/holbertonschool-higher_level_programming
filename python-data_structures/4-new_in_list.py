b164948d825e9c1ff7cc0cc7e8902aa06c36d127
aasiria24 <amaalmoasiri@gmail.com>
2026-01-18 09:31:03 +0000

    Replacing an element without modifying

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def new_in_list(my_list, idx, element):
+    """Replacing an element in a list at specific position without modifying the original list."""
+    if idx < 0 or idx >= len(my_list):
+        return my_list[:]
+
+    new_list = my_list[:]
+    newlist[idx] = element
+    return new_list
