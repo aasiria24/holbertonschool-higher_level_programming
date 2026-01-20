6151c2094bc4b9a194a319ad7ce0e70817bb9c2b
aasiria24 <amaalmoasiri@gmail.com>
2026-01-20 07:40:28 +0000

    Another code

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -2,10 +2,9 @@
 import sys
 
 def safe_print_integer_err(value):
-    """Prints an integer and handles errors by printing to stderr."""
     try:
         print("{:d}".format(value))
         return True
-    except (ValueError, TypeError) as e:
+    except Exception as e:
         print("Exception: {}".format(e), file=sys.stderr)
         return False
