3a21005a8fa462b54549146a4031306310b3a8c4
aasiria24 <amaalmoasiri@gmail.com>
2026-01-20 10:02:17 +0000

    Fixing the errors5

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -34,7 +34,7 @@ def matrix_divided(matrix, div):
 
         for element in row:
             if not isinstance(element, (int, float)):
-                msg = "matrix must be a matrix (list of lists) of integers/floats"
+                msg = "matrix must be a matrix of integers/floats"
                 raise TypeError(msg)
 
         if row_length is None:
