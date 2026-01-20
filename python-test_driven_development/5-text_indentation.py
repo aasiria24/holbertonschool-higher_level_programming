a986a77182631d2d92980b5ca054376c701fcf52
aasiria24 <amaalmoasiri@gmail.com>
2026-01-20 17:19:36 +0000

    White spaces v2

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -9,7 +9,6 @@ Prints a text with 2 new lines after each of these characters: ., ? and :
 
 def text_indentation(text):
     """Prints a text with 2 new lines after each ., ? and :
-    
     Args:
         text: The text to process (must be a string)
 
