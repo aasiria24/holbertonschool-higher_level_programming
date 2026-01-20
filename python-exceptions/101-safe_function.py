76c3b6f51e3c40a871104e10fee8ef2d9c6d9fee
aasiria24 <amaalmoasiri@gmail.com>
2026-01-20 07:35:20 +0000

    Rewriting the code

--- python-exceptions/101-safe_function.py
+++ python-exceptions/101-safe_function.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 import sys
+
 def safe_function(fct, *args):
     """Executes a function safely."""
     try:
         return fct(*args)
     except Exception as e:
-        print("Exception: {}".format(e), file=sys.stderr)
+        sys.stderr.write("Exception: {}\n".format(e))
         return None
