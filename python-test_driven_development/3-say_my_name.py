bfeaa7518b6d107353be8aa2a8cc71a088fbba51
aasiria24 <amaalmoasiri@gmail.com>
2026-01-20 16:17:54 +0000

    White spaces

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -9,7 +9,7 @@ Prints "My name is <first name> <last name>"
 
 def say_my_name(first_name, last_name=""):
     """Prints "My name is <first name> <last name>"
-    
+
     Args:
         first_name: must be a string
         last_name: must be a string
