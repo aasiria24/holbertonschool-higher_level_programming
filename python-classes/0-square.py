b56ca6244e2837732b0f23b1f409d346f0e9c530
aasiria24 <amaalmoasiri@gmail.com>
2026-01-25 09:31:54 +0000

    My first class

---
+++ python-classes/0-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module: 0-square
+This module defines an empty Square class.
+"""
+
+
+class Square:
+    """
+    An empty class that defines an empty Square class.
+
+    This class doesn't have any attributes or methods yet.
+    It serves as a blueprint for creating square objects.
+    """
+    pass
