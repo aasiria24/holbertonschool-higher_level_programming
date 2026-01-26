8fd6a751cda5cc8182d3e43bbc6ff90324b0de0f
aasiria24 <amaalmoasiri@gmail.com>
2026-01-26 20:30:04 +0300

    Update task_04_flyingfish.py

--- python-abc/task_04_flyingfish.py
+++ python-abc/task_04_flyingfish.py
@@ -1,22 +1,42 @@
 #!/usr/bin/env python3
-"""CountedIterator - iterator that counts how many items were fetched."""
+"""Task 04: Multiple Inheritance with FlyingFish"""
 
 
-class CountedIterator:
-    """Wrap an iterable's iterator and count fetched items."""
+class Fish:
+    """Fish class representing aquatic animals"""
 
-    def __init__(self, iterable):
-        self.iterator = iter(iterable)
-        self._count = 0
+    def swim(self):
+        """Print swimming behavior of fish"""
+        print("The fish is swimming")
 
-    def __iter__(self):
-        return self
+    def habitat(self):
+        """Print habitat of fish"""
+        print("The fish lives in water")
 
-    def __next__(self):
-        item = next(self.iterator)
-        self._count += 1
-        return item
 
-    def get_count(self):
-        """Return how many items have been iterated over so far."""
-        return self._count
+class Bird:
+    """Bird class representing avian animals"""
+
+    def fly(self):
+        """Print flying behavior of bird"""
+        print("The bird is flying")
+
+    def habitat(self):
+        """Print habitat of bird"""
+        print("The bird lives in the sky")
+
+
+class FlyingFish(Fish, Bird):
+    """FlyingFish class that inherits from both Fish and Bird"""
+
+    def fly(self):
+        """Override Bird's fly method for flying fish"""
+        print("The flying fish is soaring!")
+
+    def swim(self):
+        """Override Fish's swim method for flying fish"""
+        print("The flying fish is swimming!")
+
+    def habitat(self):
+        """Override habitat method to reflect dual nature"""
+        print("The flying fish lives both in water and the sky!")
