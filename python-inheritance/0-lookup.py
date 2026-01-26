1a0f9ebad134d27c625d87ec3653ed7e032c7eec
aasiria24 <amaalmoasiri@gmail.com>
2026-01-26 12:04:36 +0300

    lookup

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module containing a function to look up object attributes and methods"""
+
+
+def lookup(obj):
+    """Return a list of available attributes and methods of an object
+    
+    Args:
+        obj: Any Python object
+        
+    Returns:
+        list: List of strings containing attribute and method names
+    """
+    return dir(obj)
