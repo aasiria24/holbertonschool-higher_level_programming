4b91a69fc2e01d36180f25024c7366fc4cd29dcb
aasiria24 <amaalmoasiri@gmail.com>
2026-01-26 17:15:26 +0300

    Fixing line 6 and 13 length

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -3,13 +3,15 @@
 
 
 def is_kind_of_class(obj, a_class):
-    """Check if an object is an instance of, or inherited from, a specified class
+    """Check if an object is an instance of
+    or inherited from, a specified class
 
     Args:
         obj: The object to check
         a_class: The class to compare against
 
     Returns:
-        bool: True if obj is an instance of a_class or its subclasses, False otherwise
+        bool: True if obj is an instance of a_class or its subclasses
+        False otherwise
     """
     return isinstance(obj, a_class)
