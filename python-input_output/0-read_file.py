cfae7ddad62e90c48cfef8906f1602a048a1598b
aasiria24 <amaalmoasiri@gmail.com>
2026-02-02 07:46:49 +0000

    Reading a file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -3,6 +3,12 @@
 def read_file(filename=""):
     """
     Read a text file (UFT-8) and prints its content to stdout.
+
+    Args:
+    filename (str): The name/path of the file to read.
+
+    Returns:
+    None
     """
     with open(filename, 'r', encoding='uft-8') as file:
         for line in file:
