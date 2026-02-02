b09293fb32ceab731ec65094532b88a03f57cd76
aasiria24 <amaalmoasiri@gmail.com>
2026-02-02 09:23:39 +0000

    Load, add, save

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -13,13 +13,21 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 filename = "add_item.json"
 
 
-try:
-    item = load_from_json_file(filename)
-except FileNotFoundError:
-    item = []
+def main():
+    """
+    Main function to load existing list, add new arguments,
+    and save back to file.
+    """
+    if os.path.exists(filename):
+        my_list = load_from_json_file(filename)
+    else:
+        my_list = []
+
+    for arg in sys.argv[1:]:
+        my_list.append(arg)
+
+    save_to_json_file(my_list, filename)
 
 
-    item.extend(sys.argv[1:])
-
-
-    save_to_json_file(item, filename)
+if __name__ == "__main__":
+    main()
