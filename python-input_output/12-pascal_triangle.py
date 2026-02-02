a0f4266c5cadec7551a0ac92cfa57e3410e5fb40
aasiria24 <amaalmoasiri@gmail.com>
2026-02-02 09:44:00 +0000

    Generating Pascal's Triangle

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -20,13 +20,13 @@ def pascal_triangle(n):
     triangle = [[1]]
 
     for i in range(1, n):
-        prev_row = triangle[i -1]
+        prev_row = triangle[i - 1]
         new_row = [1]
 
-        for j in range(1, i):
+        for j in range(1, len(prev_row)):
             new_row.append(prev_row[j - 1] + prev_row[j])
 
-            new_row.append(1)
-            triangle.append(new_row)
+        new_row.append(1)
+        triangle.append(new_row)
 
     return triangle
