846d89e7a282b2cb70bbe15043a3ef9c98ae7128
aasiria24 <amaalmoasiri@gmail.com>
2026-02-23 21:30:57 +0000

    Fixing output errors

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -2,7 +2,6 @@
 import MySQLdb
 import sys
 
-
 if __name__ == "__main__":
     db = MySQLdb.connect(
         host="localhost",
@@ -17,8 +16,7 @@ if __name__ == "__main__":
     query = "SELECT * FROM states WHERE name = '{}' ORDER BY states.id ASC".format(sys.argv[4])
     cursor.execute(query)
 
-    rows = cursor.fetchall()
-    for row in rows:
+    for row in cursor.fetchall():
         print(row)
 
     cursor.close()
