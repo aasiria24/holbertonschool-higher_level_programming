9539f522fd5456b350ced8fed7e738966158bfd1
aasiria24 <amaalmoasiri@gmail.com>
2026-02-23 21:23:27 +0000

    Filter states by user input

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,26 +1,41 @@
 #!/usr/bin/python3
-"""Script that takes in an argument and displays all values in the states
-table of hbtn_0e_0_usa where name matches the argument"""
+"""
+Script that takes an argument and displays all values in the states table
+where name matches the argument.
+Usage: ./2-my_filter_states.py <mysql username> <mysql password>
+                               <database name> <state name searched>
+"""
+
 import MySQLdb
 import sys
 
-
 if __name__ == "__main__":
+    # Get command line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    # Connect to MySQL server
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
+        user=username,
+        passwd=password,
+        db=database
     )
-    cur = db.cursor()
-    cur.execute(
-        "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(
-            sys.argv[4]
-        )
-    )
-    query_rows = cur.fetchall()
-    for row in query_rows:
+
+    cursor = db.cursor()
+
+    # Create the query using format (as required)
+    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(state_name)
+
+    # Execute the query
+    cursor.execute(query)
+
+    # Fetch and print all matching rows
+    for row in cursor.fetchall():
         print(row)
-    cur.close()
+
+    cursor.close()
     db.close()
