c87af395f79a528fc4ab90d097c61a5271cbbe76
aasiria24 <amaalmoasiri@gmail.com>
2026-02-23 10:46:29 +0000

    Rotten tomatoes

--- SQL_more_queries/102-rating_shows.sql
+++ SQL_more_queries/102-rating_shows.sql
@@ -1,7 +1,10 @@
--- Lists all shows from hbtn_0d_tvshows_rate by their rating sum.
--- Displays title and rating sum, sorted descending by rating.
-SELECT tv_shows.title, COALESCE(SUM(tv_show_ratings.rating), 0) AS rating
+-- 102-rating_shows.sql
+
+SELECT
+    tv_shows.title,
+    SUM(tv_show_ratings.rate) AS rating
 FROM tv_shows
-LEFT JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
+INNER JOIN tv_show_ratings
+    ON tv_shows.id = tv_show_ratings.show_id
 GROUP BY tv_shows.id
 ORDER BY rating DESC;
