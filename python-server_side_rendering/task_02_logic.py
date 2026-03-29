9d52a3d07eecc1768c4c87a5cba48385c877ec67
aasiria24 <amaalmoasiri@gmail.com>
2026-03-29 10:01:53 +0000

    Adding data.get('items', [])

--- python-server_side_rendering/task_02_logic.py
+++ python-server_side_rendering/task_02_logic.py
@@ -23,7 +23,7 @@ def contact():
 def items():
     with open('items.json', 'r') as f:
         data = json.load(f)
-    return render_template('items.html', items=data['items'])
+    return render_template('items.html', items=data.get('items', []))
 
 
 if __name__ == '__main__':
